587504ad6d71341bcb0ab6504d9837d37e033ab3
Zmj <zmjzmjzmj33@sina.com>
2018-01-23 11:20:06 +0800

    第一次上传

---
+++ src/main/java/com/zmj/wkt/WktApplication.java
@@ -0,0 +1,12 @@
+package com.zmj.wkt;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WktApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WktApplication.class, args);
+	}
+}
---
+++ src/test/java/com/zmj/wkt/WktApplicationTests.java
@@ -0,0 +1,16 @@
+package com.zmj.wkt;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class WktApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
